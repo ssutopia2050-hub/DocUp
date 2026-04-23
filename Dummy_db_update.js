475079f2de8552fb59f8a07a1550517792f75fd5
Shikhar <ssutopia2050@gmail.com>
2026-04-24 00:29:48 +0530

    empty state improvement

--- Dummy_db_update.js
+++ Dummy_db_update.js
@@ -9,7 +9,7 @@ async function run() {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
 
-        const result = await User.updateMany(
+        const result = await doc.updateMany(
             {},
             { $set: { reviewed: true } },
         );
